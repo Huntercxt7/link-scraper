69ba0d3e6cc2955b8d591fb6f6166ec6d910c5bd
Huntercxt7 <sayadnazari88@gmail.com>
2026-05-11 23:48:35 +0330

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Site Viewer</title>
+</head>
+<body>
+
+<h2>Website Viewer</h2>
+
+<iframe 
+src="https://example.com"
+width="100%"
+height="800px">
+</iframe>
+
+</body>
+</html>
