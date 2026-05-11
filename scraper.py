6336a8a16b7c81a1f3f0d0d94f3b3671e08acbe4
Huntercxt7 <sayadnazari88@gmail.com>
2026-05-11 23:52:40 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,8 @@
 <h2>Website Viewer</h2>
 
 <iframe 
-src="https://example.com"
+src="https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-dotnettools/vsextensions/csharp/1.26.0/vspackage
+"
 width="100%"
 height="800px">
 </iframe>
